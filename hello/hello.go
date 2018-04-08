0f50063002d16e9f5a4b6ab15c09027a8b2b06eb
Vladislav Shabarov <st.kfem@gmail.com>
2018-04-08 17:05:17 +0300

    reverse_test

--- hello/hello.go
+++ hello/hello.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 
 	"github.com/kfeun/gotestrepos/stringutil"
-) 
-
+)
 
 func main() {
-	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf(stringutil.Reverse("\n!oG ,olleH"))
 }
